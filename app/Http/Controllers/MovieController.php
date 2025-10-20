6e443678cb00c61ea44d334297bc7ea24f525702
Nathan123-code <nathan6625@gmail.com>
2025-10-20 20:22:46 +0700

    bug fixing

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,32 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use App\Models\MovieCategory;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class MovieController extends Controller
 {
-
-    // buat home
+    //untuk home
     public function home()
     {
         return view('home');
     }
 
-    // buat nampilin list film
+    //list film
     public function index()
     {
-        $movies = Movie::latest()->get();
+        $movies = Movie::all();
         return view('movies.index', ['movies' => $movies]);
     }
 
-    // ke show detail
-    public function show(Movie $movie)
+    //untuk show detail
+    public function show($id) 
     {
+        //cari pake ID
+        $movie = Movie::findOrFail($id);
         $movie->load('userReviews');
         $reviewCount = $movie->userReviews->count();
         $averageRating = $reviewCount > 0 ? $movie->userReviews->avg('rating') : 0;
+
         return view('movies.show', [
             'movie' => $movie,
             'reviewCount' => $reviewCount,
@@ -37,24 +38,26 @@ class MovieController extends Controller
         ]);
     }
 
-    // untuk list kategori
+    //buat list kategori
     public function categories()
     {
         $categories = MovieCategory::all();
         return view('movies.category', ['categories' => $categories]);
     }
 
-    // list film dari kategori
-    public function showByCategory(MovieCategory $category)
+    //nampilin film dari kategorinya
+    public function showByCategory($id)
     {
+        //nyari kategori pake id
+        $category = MovieCategory::findOrFail($id);
         $movies = $category->movies()->get();
         return view('movies.by_category', [
             'category' => $category,
-            'movies' => $movies
+            'movies' => $movies,
         ]);
     }
 
-    // buat about
+    //about us
     public function about()
     {
         return view('about');
--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,13 @@ Route::get('/', [MovieController::class, 'home'])->name('home');
 Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
 
 // detail film
-Route::get('/movie/{movie}', [MovieController::class, 'show'])->name('movies.show');
+Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movies.show');
 
 // list kategori
 Route::get('/categories', [MovieController::class, 'categories'])->name('movies.categories');
 
 // film per kategori
-Route::get('/category/{category}', [MovieController::class, 'showByCategory'])->name('movies.by_category');
+Route::get('/category/{id}', [MovieController::class, 'showByCategory'])->name('movies.by_category');
 
 // About Us
 Route::get('/about', [MovieController::class, 'about'])->name('about');
